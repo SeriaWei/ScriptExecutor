7eb54936246aaf35532a6fdfcf6509744428a5c1
Wayne <seriawei@outlook.com>
2018-11-27 09:35:22 +0800

    init

---
+++ Executor/Command.sql
@@ -0,0 +1,19 @@
+﻿
+		INSERT INTO Permission
+		        ( PermissionKey ,
+		          RoleId ,
+		          Title ,
+		          Description ,
+		          Module ,
+		          Status
+		        )
+		VALUES  ( 'EventViewer_Manage' , 
+		          1 , 
+		          '查看错误日志' , 
+		          NULL , 
+		          '设置' ,
+		          NULL
+		        );
+
+ALTER TABLE CMS_Zone ADD HeadingCode NVARCHAR(100) NULL;
+UPDATE CMS_Zone SET HeadingCode = ID;
---
+++ Executor/Program.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.IO;
+
+namespace Executor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dir = Directory.GetCurrentDirectory();
+            string command = File.ReadAllText(Path.Combine(dir, "Command.sql"), System.Text.Encoding.UTF8);
+
+            using (StreamReader reader = new StreamReader(new FileStream(Path.Combine(dir, "DbPath.txt"), FileMode.Open), System.Text.Encoding.UTF8))
+            {
+                string dbPah = reader.ReadLine();
+                while (dbPah != null)
+                {
+                    Console.WriteLine(dbPah);
+                    var dbs = Directory.GetFiles(dbPah, "*.sqlite");
+                    foreach (var item in dbs)
+                    {
+                        Console.WriteLine(item);
+                        try
+                        {
+                            Console.WriteLine(new SqliteDbContext(item).Database.ExecuteSqlCommand(command));
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine(ex);
+                            Console.WriteLine("Press any key to continue...");
+                            Console.ReadKey();
+                        }
+                    }
+                    dbPah = reader.ReadLine();
+                }
+
+            }
+        }
+    }
+    class SqliteDbContext : DbContext
+    {
+        public string DatabaseFile { get; set; }
+        public SqliteDbContext(string file)
+        {
+            DatabaseFile = file;
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=" + DatabaseFile);
+        }
+    }
+}
